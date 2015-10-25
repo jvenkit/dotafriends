9568bb332fa2a1347329eecfd925f860c50ca46f
justin <justin.venkit@gmail.com>
2015-10-25 21:57:54 +0800

    Remove description template

--- app/src/main/java/com/dotafriends/dotafriends/models/PlayerSummaries.java
+++ app/src/main/java/com/dotafriends/dotafriends/models/PlayerSummaries.java
@@ -2,9 +2,6 @@ package com.dotafriends.dotafriends.models;
 
 import java.util.List;
 
-/**
- * Created by justin on 19/10/2015.
- */
 public class PlayerSummaries {
     private List<PlayerSummary> players;
 
